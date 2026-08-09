814a106a520c04599e5af4b34c7cc946c7a0bd33
sreedhar6247-commits <sreedhar6247@gmail.com>
2026-08-09 09:45:48 +0530

    Update store.ts

--- lib/store.ts
+++ lib/store.ts
@@ -1,4 +1,4 @@
 export const store = {
-  name: process.env.NEXT_PUBLIC_STORE_NAME ?? "BEG Shopping",
-  upiId: process.env.NEXT_PUBLIC_UPI_ID ?? "begshopping@upi"
+  name: process.env.NEXT_PUBLIC_STORE_NAME ?? "BEE GIRL Shopping",
+  upiId: process.env.NEXT_PUBLIC_UPI_ID ?? "yourupi@bank",
 };
