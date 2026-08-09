325a54974c4c646028deeaf01b4c449020d9b738
sreedhar6247-commits <sreedhar6247@gmail.com>
2026-08-09 09:21:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   title: "Bee Girl Shopping",
   description: "Women's Clothing",
   verification: {
-    google: "xIC2GuXgH7p_ucrAS1wJTvIQBao9sj7cfUteihTcCmw",
+    google: "xiC2GuXgH7p_ucrAS1wJTvlQBao9sj7cfUteihTeCmw",
   },
 };
 
